c04aa72c545a02a5a42b2428386219358ab08c37
emilio <ermsharo@gmail.com>
2023-01-05 02:59:28 -0300

    update our struct changes

--- my-app/src/components/Board.js
+++ my-app/src/components/Board.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import About from "./About";
+import Footer from "./Footer";
 
 function Board() {
   return (
-    <div className="App">
-      Board here
-    </div>
+    <>
+      <Header/>
+      <About/>
+      <Footer/>
+    </>
   );
 }
 
