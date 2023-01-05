6e729c41732efecede02673cc43157b40e4c1ba4
emilio <ermsharo@gmail.com>
2023-01-05 06:47:44 -0300

    update our font

--- my-app/src/components/Board.js
+++ my-app/src/components/Board.js
@@ -2,13 +2,25 @@ import Header from "./Header";
 import About from "./About";
 import Footer from "./Footer";
 
+import styled from 'styled-components'
+
+const BoardBox = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100&display=swap');
+background-color: #1f1f1f;
+  min-height: 100vh;
+  width: 100%;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  font-family: 'JetBrains Mono', monospace;
+`
+
 function Board() {
   return (
-    <>
+    <BoardBox>
       <Header/>
       <About/>
       <Footer/>
-    </>
+    </BoardBox>
   );
 }
 
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,10 +1,41 @@
+import styled from 'styled-components'
+
+const HeaderBox = styled.div`
+
+
+
+
+`
+
+const OptionsDisplay = styled.ul`
+
+display: flex ;
+width: 80%;
+justify-content: space-between;
+margin:auto; 
+padding: calc(10px + 2vmin);
+list-style-type: none;
+
+li{
+  :hover{
+    color:red;
+  }
+}
+`
+
 
 
 function Header() {
   return (
-    <div className="App">
-      Header
-    </div>
+    <HeaderBox>
+      <OptionsDisplay>
+
+        <li>Home</li>
+        <li>Sobre</li>
+        <li>Projetos</li>
+        <li>Curriculo</li>
+      </OptionsDisplay>
+    </HeaderBox>
   );
 }
 
