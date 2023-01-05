50cf0a03e5258e703731bfe453a8243e4b196a7f
emilio <ermsharo@gmail.com>
2023-01-05 07:47:16 -0300

    update or changes

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,17 +1,40 @@
 import styled from "styled-components";
 
 const ProjectsBox = styled.div`
-  text-align: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  width: 100%;
+  grid-gap: 32px;
 `;
 
 const ProjectCard = styled.div`
   text-align: center;
+  background-color: #242435;
+  border-radius: 16px;
+  padding: 32px;
+`;
+
+const ProjectDescription = styled.div`
+  text-align: left;
+  text-transform: capitalize;
+`;
+
+const ProjectImage = styled.div`
+  margin: auto;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+  }
 `;
 
 const fillProject = () => {
   return {
     name: "generic",
     stacks: "stack a, stack b, stack c",
+    image:
+      "https://img.freepik.com/fotos-premium/cachorrinho-fofo-de-spitz-pomeranian-deitado-no-fundo-amarelo-brilhante_253512-22.jpg?w=2000",
   };
 };
 
@@ -20,7 +43,20 @@ let projetos = [];
 for (let i = 0; i < 5; i++) projetos.push(fillProject());
 
 function Projects() {
-  return <ProjectsBox>projects here</ProjectsBox>;
+  return (
+    <ProjectsBox>
+      {projetos.map((project) => {
+        return (
+          <ProjectCard>
+            <ProjectImage>
+              <img src={project.image} />
+            </ProjectImage>
+            <ProjectDescription>{project.name}</ProjectDescription>
+          </ProjectCard>
+        );
+      })}
+    </ProjectsBox>
+  );
 }
 
 export default Projects;
