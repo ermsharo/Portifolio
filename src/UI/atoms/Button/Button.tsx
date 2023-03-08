cc797907806d25c5115278532e4a30dcd839f446
Emilio Haro <ermsharo@gmail.com>
2023-03-08 12:20:16 -0300

    add changes

---
+++ src/UI/atoms/Button/Button.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export interface ButtonProps {
+
+    children: string | JSX.Element | JSX.Element[];
+
+
+}
+
+export const ButtonBox = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  background-color: darkblue;
+  color: white;
+  width: calc(100%);
+  height: 3rem;
+  margin: auto;
+
+  margin-bottom: 2rem;
+  &:hover {
+
+  }
+`;
+
+
+function Button({ }: ButtonProps) {
+    return <ButtonBox>Button</ButtonBox>;
+};
+
+export default Button;
--- src/UI/organism/ContactForm/ContactForm.tsx
+++ src/UI/organism/ContactForm/ContactForm.tsx
@@ -1,8 +1,53 @@
 import React, { useState } from "react";
 import { send } from "emailjs-com";
 import { MAIL_TEAMPLATE_ID, MAIL_SERVICE_ID ,USER_ID} from "./../../../env";
+import styled from "styled-components";
 
 export interface ContactFormProps {}
+
+export const ButtonBox = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  background-color: darkblue;
+  color: white;
+  width: calc(100%);
+  height: 3rem;
+  margin: auto;
+
+  margin-bottom: 2rem;
+  &:hover {
+
+  }
+`;
+
+
+export const Input = styled.input`
+
+  font-size: 1.2rem;
+  border-radius: 0.2rem;
+  border: 0;
+  box-sizing: border-box;
+  width: calc(100%);
+  margin-top: 0.5rem;
+  padding: 1rem;
+  margin-bottom: 1rem;
+
+  &::placeholder {
+
+  }
+  &:focus{
+
+    outline-style: dashed;
+  }
+`;
+
 
 function ContactForm({}: ContactFormProps) {
   const [sendError, setSendError] = useState<boolean>(false);
@@ -35,28 +80,28 @@ function ContactForm({}: ContactFormProps) {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input
+        <Input
           type="text"
           name="from_name"
           placeholder="from name"
           value={toSend.from_name}
           onChange={handleChange}
         />
-        <input
+        <Input
           type="text"
           name="message"
           placeholder="Your message"
           value={toSend.message}
           onChange={handleChange}
         />
-        <input
+        <Input
           type="text"
           name="reply_to"
           placeholder="Your email"
           value={toSend.reply_to}
           onChange={handleChange}
         />
-        <button type="submit">Submit</button>
+        <ButtonBox type="submit">Submit</ButtonBox>
       </form>
       {sendError && <> Erro ao enviar</>}
     </div>
